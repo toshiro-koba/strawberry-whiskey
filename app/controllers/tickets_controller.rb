4149b63dafffe22ed59e38fb10d0d0e172402b55
toshiro-koba <kobato.231.0625@gmail.com>
2022-07-04 23:24:01 +0900

    タスクを登録できる #3

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,20 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.all
   end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    ticket = Ticket.new(ticket_params)
+    ticket.save!
+    redirect_to(tickets_url, notice: "タスク「#{ticket.title}」を登録しました。")
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :body) # TODO: 締切日も許可する
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "tickets#index"
+  resources :tickets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
