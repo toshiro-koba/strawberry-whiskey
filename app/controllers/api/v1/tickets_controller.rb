d1b8e1729c926ddd02c23807844f3d05d3ae1007
toshiro-koba <kobato.231.0625@gmail.com>
2022-07-18 09:26:06 +0900

    一覧取得外部APIは今は不要 #13

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,11 +1,4 @@
 class Api::V1::TicketsController < ApiBaseController
-  def index
-    self.status = :ok
-
-    tickets = Ticket.all
-    self.response_body = tickets.to_json
-  end
-
   def completion_status
     ticket = Ticket.find(params[:id])
     ticket.update!(is_completed: params[:status])
--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,6 +1,5 @@
 namespace :api do
   namespace :v1 do
-    get  '/tickets', to: 'tickets#index'
     post '/tickets/completion_status', to: 'tickets#completion_status'
   end
 end
