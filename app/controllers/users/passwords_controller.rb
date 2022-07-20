d29b65ecdc42665d28d9df17ed1469929567962b
toshiro-koba <kobato.231.0625@gmail.com>
2022-07-21 00:20:56 +0900

    devise 関連の各種コントローラーを生成 #10

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Users::PasswordsController < Devise::PasswordsController
+  # GET /users/password/new
+  def new
+    super
+  end
+
+  # POST /users/password
+  def create
+    super
+  end
+
+  # GET /users/password/edit?reset_password_token=abcdef
+  def edit
+    super
+  end
+
+  # PUT /users/password
+  def update
+    super
+  end
+
+  # protected
+
+  # def after_resetting_password_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sending reset password instructions
+  # def after_sending_reset_password_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+end
